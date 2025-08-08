9894a75bc3fd54dbb4d773c483aca4d735ed6a27
SabyJeany <Sabyjeany@gmail.com>
2025-08-08 23:35:41 +0200

    feat: implement binary_tree_uncle to find the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,27 +1,17 @@
-c
-
-
 #include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
 
 /**
- * binary_tree_node - Creates a binary tree node
- * @parent: Pointer to the parent node
- * @value: Value to store in the new node
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
  *
- * Return: Pointer to the new node, or NULL if malloc fails
+ * Return: a pointer to the uncle node
+ * If node is NULL, return NULL
+ * If node has no uncle, return NULL
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    binary_tree_t *node;
-    node = malloc(sizeof(binary_tree_t));
-    if (!node)
-	    return (NULL);
-    node->n = value;
-
-    node->parent = parent;
-
-    node->left = NULL;
-    node->right = NULL;
-
-    return (node);
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return (NULL);
+return (binary_tree_sibling(node->parent));
 }
